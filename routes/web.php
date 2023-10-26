b40938c84f1c175e667c1a6c5322f4c235a60037
AcepImam21 <Acepimam19@gmail.com>
2023-10-26 23:10:52 +0700

    3. Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Imam H",
+        "email" => "acepimam19@gmail.com",
+        "image" => "profile.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
+});
+
+
