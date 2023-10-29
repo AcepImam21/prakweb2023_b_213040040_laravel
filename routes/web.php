f42bb0b08acb647e023642ef11776d5b816e7057
AcepImam21 <Acepimam19@gmail.com>
2023-10-29 23:52:31 +0700

    5. Model, Collection & Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Models\Post;
+use PhpParser\Node\Expr\FuncCall;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -31,56 +35,7 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Posts Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Imam H",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus aut laborum tempore corrupti harum maiores aspernatur consectetur doloribus reiciendis exercitationem molestias natus cumque doloremque, placeat aliquam saepe quod ipsum nesciunt."
-        ],
-        [
-            "title" => "Judul Posts Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Dafran",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel asperiores non molestias, quod itaque libero! Blanditiis amet consequuntur sapiente totam itaque! Commodi quibusdam vitae aliquam sint sapiente rem perspiciatis iste dolor velit dolorum aliquid adipisci nemo veniam illum, nulla nobis! Quia itaque adipisci quas quam eum ducimus natus veritatis atque nihil blanditiis iure perspiciatis voluptas ex odio sed tenetur, optio cum eos incidunt. Impedit aperiam repellat quam eius culpa soluta exercitationem non tempore veritatis, voluptatem aliquam, dicta quasi. Dolores maxime, delectus non nisi enim ex vel maiores fugit. Minus molestiae vel molestias qui temporibus voluptatem odit excepturi iure cupiditate ex!"
-        ],
-    ];
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-
-// halaman single post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Posts Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Imam H",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus aut laborum tempore corrupti harum maiores aspernatur consectetur doloribus reiciendis exercitationem molestias natus cumque doloremque, placeat aliquam saepe quod ipsum nesciunt."
-        ],
-        [
-            "title" => "Judul Posts Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Dafran",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel asperiores non molestias, quod itaque libero! Blanditiis amet consequuntur sapiente totam itaque! Commodi quibusdam vitae aliquam sint sapiente rem perspiciatis iste dolor velit dolorum aliquid adipisci nemo veniam illum, nulla nobis! Quia itaque adipisci quas quam eum ducimus natus veritatis atque nihil blanditiis iure perspiciatis voluptas ex odio sed tenetur, optio cum eos incidunt. Impedit aperiam repellat quam eius culpa soluta exercitationem non tempore veritatis, voluptatem aliquam, dicta quasi. Dolores maxime, delectus non nisi enim ex vel maiores fugit. Minus molestiae vel molestias qui temporibus voluptatem odit excepturi iure cupiditate ex!"
-        ],
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
 
 
