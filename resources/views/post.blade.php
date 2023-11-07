20c9d1b893129ae2ce21b71432729a83d0d9c35f
AcepImam21 <Acepimam19@gmail.com>
2023-11-07 17:05:20 +0700

    12. Redesign UI

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,15 +2,27 @@
 @extends('layouts.main')
 
 @section('container')
-    <article>
-        <h2>{{ $post->title }}</h2>
 
-        <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</p></a>
+<div class="container">
+    <div class="row justify-content-center mb-5">
+        <div class="col-md-8">
+            <h1 class="mb-3">{{ $post->title }}</h1>
 
-        {!! $post->body !!}
+            <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</p></a>
 
-    </article>
+            <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
 
-    <a href="/blog" class="d-block mt-5">Back To Post</a>
+            <article class="my-3 fs-5">
+                {!! $post->body !!}
+            </article>
+
+             <a href="/blog" class="d-block mt-5">Back To Post</a>
+        </div>
+    </div>
+</div>
+
+       
+
+        
 @endsection
 
